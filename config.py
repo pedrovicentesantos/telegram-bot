119b8e1d0be778d92bf14ec0bb63ddecfc9aee66
Pedro <pedro.vicentee26@gmail.com>
2018-10-20 16:37:44 -0300

    Adicionado Titans

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ names = ['The Pirate Filmes HD','Legendas.tv - Destaques - Todos','RapidMoviez',
 parameters={"quality": "1080p","limit": 50}
 yts = 'https://yts.am/api/v2/list_movies.json'
 
-series = ['Cloak','Theory','Horror','Sheldon','Affair','Suits', 'Gifted', 'Us', 'Empire', 'Modern','Murder','Flash','Supergirl','Arrow','Legends','Outlander','Vikings'];
+series = ['Cloak','Theory','Horror','Sheldon','Affair','Suits', 'Gifted', 'Us', 'Empire', 'Modern','Murder','Flash','Supergirl','Arrow','Legends','Outlander','Vikings','Titans'];
 dublados = ['Incriveis','Hotel'];
 legendados = ['Mamma','Ocean','Wasp','Disobedience'];
 formato = ['720p','x265'];
