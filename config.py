0b178822a7593f56af6d051887ed4deb8fa21258
Pedro Vicente <pedrovicente@poli.ufrj.br>
2019-06-09 12:58:43 -0300

    Add novas series

--- config.py
+++ config.py
@@ -7,7 +7,8 @@ urls_sites = ['http://rmz.cr']
 legendas = ['the.perfectionists','this.is.us','supergirl','modern.family','the.magicians',
             'gotham','legacies','brooklyn','arrow','the.flash','doom.patrol','empire',
             'the.good.fight','jane.the.virgin','legends.of.tomorrow','cloak.and.dagger',
-            'young.sheldon','the.big.bang.theory','marvels.agents.of.s.h.i.e.l.d']
+            'young.sheldon','the.big.bang.theory','marvels.agents.of.s.h.i.e.l.d',
+            'the.handmaids.tale','swamp.thing']
 
 # formato = ['720p web hevc x265-rmteam','720p hdtv hevc x265-rmteam','720p webrip hevc x265-rmteam']
 # O formato 720p WEBRip HEVC x265-RMTeam é apenas para o Doom Patrol
@@ -19,7 +20,7 @@ series_formato1 = ['the perfectionists','legacies','the magicians','brooklyn','g
                    'arrow','jane the virgin','legends of tomorrow','cloak and dagger',
                    'young sheldon','the big bang theory','marvels agents of s h i e l d']
 
-series_formato2 = ['doom patrol','the good fight']
+series_formato2 = ['doom patrol','the good fight', 'the handmaids tale','swamp thing']
 
 formato1 = ['720p web hevc x265-rmteam','720p hdtv hevc x265-rmteam']
 
